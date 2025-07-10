4e2000c631759ca926d421c7cf613ade24a851df
Jérôme Devy <jerome.devy-ext@pole-emploi.fr>
2025-07-10 18:44:42 +0200

    ci(): remet pwa

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,32 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   base: '/domandj/',
   plugins: [
-    vue()
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt'],
+      manifest: {
+        name: 'Plan de TP',
+        short_name: 'TP',
+        description: 'Application pour organiser les élèves',
+        theme_color: '#186efa',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+
   ],
 })
